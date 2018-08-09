1df824479ff3d70351ccd3bb11bf0f305d07779f
Roman Shustrov <r.shustrov@4slovo.ru>
2018-08-09 16:34:25 +0300

    final beta

--- tmp/3c35618d91b4f3376b3aee506b61e51c.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class expression3c35618d91b4f3376b3aee506b61e51c
-{
-    public static function expressionFunction3c35618d91b4f3376b3aee506b61e51c($functionList,$variableList)
-    {
-        return ((int)5 + ($variableList->get('c')->getValue() + (int)1));
-    }
-}
