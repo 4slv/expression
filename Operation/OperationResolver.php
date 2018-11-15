bdea396a60cb49be57a76e98ea9c2e8faccaa146
Alexey Dobrynin <gopooop@gmail.com>
2018-11-15 09:31:50 +0300

    tmp

--- Operation/OperationResolver.php
+++ Operation/OperationResolver.php
@@ -9,6 +9,20 @@ class OperationResolver
      * @param string $code псевдо код операции */
     public function resolve($code)
     {
+        foreach ($this->getSignList() as $operationName => $signRegexp)
+        {
+            $operationRegexp = '/^(.*)('. $signRegexp. ')(.*)$/';
+            if(preg_match($operationRegexp, $code)){
 
+            }
+        }
+    }
+
+    /**
+     * @return string[] асоциативный массив: название операции => регулярное выражение
+     */
+    protected function getSignList()
+    {
+        return OperationSignRegexp::getConstants();
     }
 }
