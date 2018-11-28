65d831df949676bc94f55e57514e1fdc07435ab4
Alexey Dobrynin <gopooop@gmail.com>
2018-11-28 10:45:59 +0300

    tmp

--- Code/CodeExecutor.php
+++ Code/CodeExecutor.php
@@ -31,6 +31,10 @@ class CodeExecutor
 
     public function execute(): void
     {
-        //$this->createCodeBlock()->setCode($this->getCode())->parse($this->getCodeContext())
+        eval($this
+            ->createCodeBlock()
+            ->setCode($this->getCode())
+            ->parse($this->getCodeContext())
+            ->getPhp());
     }
 }
